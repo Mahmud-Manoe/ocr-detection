770625f56995e670553610da262584d8f5399513
Mahmud-Manoe <79903974+Mahmud-Manoe@users.noreply.github.com>
2024-05-29 22:51:27 +0700

    add REAMDMI.md

---
+++ ocr-scan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ocr-scan
+//
+//  Created by Mahmud Manoe on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ocr-scan/ocr_scanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ocr_scanApp.swift
+//  ocr-scan
+//
+//  Created by Mahmud Manoe on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ocr_scanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
