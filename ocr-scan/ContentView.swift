828273c8c7b35f33140c9ab4d10f155c34f4ce7c
Mahmud-Manoe <79903974+Mahmud-Manoe@users.noreply.github.com>
2024-05-29 23:36:25 +0700

    add text

--- ocr-scan/ContentView.swift
+++ ocr-scan/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Hello, abangku😱!")
+            Text("Hello, adekuku😱!")
         }
         .padding()
     }
